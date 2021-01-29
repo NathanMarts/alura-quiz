c575305fad0a10a3da184515d94988c88f0e1863
Natã Lopes Martins <natamartins@pmni>
2021-01-29 10:58:05 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import db from '../db.json';
 
+const { theme } = db;
+
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
@@ -13,7 +15,7 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
     font-family: 'Lato', sans-serif;
-    color: ${({ theme }) => theme.colors.contrastText};
+    color: ${theme.colors.contrastText};
   }
   html, body {
     min-height: 100vh;
@@ -24,8 +26,6 @@ const GlobalStyle = createGlobalStyle`
     flex-direction: column;
   }
 `;
-
-const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
